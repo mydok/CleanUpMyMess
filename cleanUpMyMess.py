189f137e05d72c020645afe5a5f3e2d453b908ba
mydok <danesalvarez@gmail.com>
2019-07-31 00:24:08 +0200

    Simplified imports. Cleanup unrelated comments. Translated error messages

--- cleanUpMyMess.py
+++ cleanUpMyMess.py
@@ -1,13 +1,15 @@
-import glob, os, sys, time
+from time import asctime,localtime,time
+from sys import exit, argv
+from os import path, makedirs, remove
+from glob import glob
 from pprint import pprint
 from shutil import copyfile
 from pathlib import Path
 
 # USAGE
-# ordenarFicherosMasivo.py path [niveles] [ficheros por nivel]
+# ordenarFicherosMasivo.py path [niveles]
 # path: directorio sobre el que se quiere realizar la ordenación
 # niveles: niveles de ordenación que se crearan. Por defecto 1
-# ficheros por nivel: Si no se recibe este parametro, se creará una carpeta por cada vocal. En caso de recibirlo, se intenta ajustar respetando el numero de ficheros, siempre respetando las letras completas
 
 # Definimos el array que usaremos para iterar en la creación de carpetas
 
@@ -15,7 +17,7 @@ specialChars = [',','\'','=','.','-','_',' ','0','1','2','3','4','5','6','7','8'
 folderIterator = specialChars.copy() + ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 
-def orderFolder(path, level):
+def orderFolder(apath, level):
     
     # Creamos las carpetas
     for folderName in folderIterator:
@@ -24,49 +26,49 @@ def orderFolder(path, level):
             char = '#'
         else:
             char = folderName
-        realFolderName = os.path.join(path,char)
-        #searchFiles = os.path.join(path, folderName)
-        #No funciona para los numeros. preparar para que si llega el caracter # busque numeros
-        
-        files = glob.glob(path+os.path.sep+folderName.rjust(level+1,'?')+'*')
+        realFolderName = path.join(apath,char)
+        files = glob(apath+path.sep+folderName.rjust(level+1,'?')+'*')
 
         if len(files) > 0:                
-            if not os.path.exists(realFolderName):
-                os.makedirs(realFolderName)
+            if not path.exists(realFolderName):
+                makedirs(realFolderName)
             for aFile in files:
-                if os.path.isdir(aFile):
+                if path.isdir(aFile):
                     continue
                 fileSrcName = Path(aFile).name
-                auxSrcFile = os.path.join(path,fileSrcName)
-                auxdestFile = os.path.join(realFolderName,fileSrcName)
+                auxSrcFile = path.join(apath,fileSrcName)
+                auxdestFile = path.join(realFolderName,fileSrcName)
                 copyfile(auxSrcFile,auxdestFile)
-                os.remove(auxSrcFile)
+                remove(auxSrcFile)
 
 
 #comprobamos que, minimo tiene que venir un argumento
-if len(sys.argv) <=1:
-    print ("Número de argumentos invalido. Es necesario indicar el path")
-    sys.exit(1)
+if len(argv) <=1:
+    print ("Invalid argument number. Path is mandatory")
+    exit(1)
 else:
-    path = sys.argv[1]
+    mypath = argv[1]
     niveles = 1
-    if len(sys.argv) == 3:
-        if not sys.argv[2].isnumeric():
-            print ("El segundo parámetro debe ser numérico")
-            sys.exit(3)
-        niveles = int(sys.argv[2])
+    if len(argv) == 3:
+        if not argv[2].isnumeric():
+            print ("Deep argument must be numeric")
+            exit(3)
+        niveles = int(argv[2])
         if niveles > 3:
-            print ("Demasiados subniveles")
-            sys.exit(4)
+            print ("Too many deep")
+            exit(4)
+        elif niveles < 1:
+            print ("Min deep must be 1")
+            exit(5)
 
-    if os.path.exists(path) == False:
-        print("El path no es valido")
-        sys.exit(2)
+    if path.exists(mypath) == False:
+        print("Invalid path")
+        exit(2)
     else:
-        localtime = time.asctime( time.localtime(time.time()) )
-        print ("Inicio del script: "+localtime)
+        mytime = asctime(localtime(time()) )
+        print ("Script starts at: "+mytime)
         vueltas  = 0
-        arrPath = [path]
+        arrPath = [mypath]
         auxArrPath = []        
         while vueltas < niveles:
             # hacemos los path
@@ -82,10 +84,10 @@ else:
                             continue
                     else:
                         char = letter
-                    auxArrPath.append(os.path.join(folder, char))
+                    auxArrPath.append(path.join(folder, char))
             arrPath = auxArrPath
             auxArrPath = []   
             vueltas = vueltas +1  
-        localtime = time.asctime( time.localtime(time.time()) )
-        print ("Fin del script: "+localtime)
-        sys.exit(0)
+        mytime = asctime(localtime(time()))
+        print ("Script ends at: "+mytime)
+        exit(0)
